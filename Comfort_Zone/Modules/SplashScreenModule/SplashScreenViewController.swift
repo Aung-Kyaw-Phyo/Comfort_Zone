4519242c473fb81a7b721b7e32b8acf424029e77
Aung Kyaw Phyo <Dev.aungkyawphyo@gmail.com>
2024-03-01 01:39:47 +0630

    create splash screen and home screen

---
+++ Comfort_Zone/Modules/SplashScreenModule/SplashScreenViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SplashScreenViewController.swift
+//  Comfort_Zone
+//
+//  Created by Aung Kyaw Phyo on 01/03/2024.
+//
+
+import UIKit
+
+class SplashScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
